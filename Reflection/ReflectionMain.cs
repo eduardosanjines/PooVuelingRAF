04e50b5ae64a24f96faef81df607245ea95baa51
EduardoSanjines <eduardosanjines@gmail.com>
2018-06-29 00:24:16 +0200

    provando objetos xml lectura

--- Reflection/ReflectionMain.cs
+++ Reflection/ReflectionMain.cs
@@ -1,10 +1,12 @@
 ﻿using Reflection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Reflection
 {
@@ -15,6 +17,8 @@ namespace Reflection
 
             Assembly myAssembly = typeof(ReflectionMain).Assembly;
 
+            XmlTextReader reader = new XmlTextReader("Alumno.xml");
+            
             Type alumnoType = myAssembly.GetType("Reflection.Alumno");
 
             object objetoAlumno = Activator.CreateInstance(alumnoType, 1, "Pepe", "Soto", "123456E");
