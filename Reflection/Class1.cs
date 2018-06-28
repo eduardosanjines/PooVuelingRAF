d32115c42feb566a592c3c752c97e8c7b87a7c07
EduardoSanjines <eduardosanjines@gmail.com>
2018-06-28 17:03:18 +0200

    ejercicio AutoMapper cambio de nombre

---
+++ Reflection/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Class1
+    {
+        private int Documento;
+        private String Dni;
+        private String Apellido;
+
+
+
+        DateTime dateTime = (DateTime)Activator.CreateInstance(typeof(DateTime));
+
+
+    }
+}
--- Reflection/Properties/AssemblyInfo.cs
+++ Reflection/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("AutoMapper")]
+[assembly: AssemblyTitle("Reflection")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("AutoMapper")]
+[assembly: AssemblyProduct("Reflection")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("2ab4c9aa-8876-4b2a-817a-af12e054bc9a")]
+[assembly: Guid("bd46df7c-e691-4f04-ad4f-9d3ea065da20")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
